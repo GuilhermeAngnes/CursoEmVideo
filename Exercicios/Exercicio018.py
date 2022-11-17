3f1581b7ad474211b26abcb8c8090e3a3d442d7a
GuilhermeAngnes <guilhermeangnes13@gmail.com>
2022-11-16 23:12:38 -0300

    Exercicio de Seno Cosseno e Tangente

--- Exercicios/Exercicio018.py
+++ Exercicios/Exercicio018.py
@@ -5,5 +5,13 @@ import math
 
 angulo = float(input('Informe um ângulo qualquer: '))
 seno = math.sin(math.radians(angulo))
+print('O Angulo {} tem o SENO de {:.2f}'.format(angulo, seno))
 
-resul = ('')
+
+cosseno = math.cos(math.radians(angulo))
+result_cosseno = ('O angulo {} tem COSSENO {:.2f}'.format(angulo, cosseno))
+print(result_cosseno)
+
+tangente = math.tan(math.radians(angulo))
+result_tange = ('O angulo {} tem TANGENTE {:.2f}'.format(angulo, tangente))
+print(result_tange)
