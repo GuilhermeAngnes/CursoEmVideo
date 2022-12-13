7ce34803e90df229faed21623ed23a007203ddf7
GuilhermeAngnes <guilhermeangnes13@gmail.com>
2022-12-12 22:12:22 -0300

    Aula09 - Manipulando Textos (Caracteres)

---
+++ Aulas/Aula09.py
@@ -0,0 +1,24 @@
+"""
+1 - Qualquer frase, em programação, é considerado uma cadeia de caracteres.
+"""
+"""
+2 - No fatiamento de String, o ultimo valor não entra na contagem, ex: [1:5] apenas entra o 1,2,3,4.
+"""
+"""
+upper - Torna todas as letras em maiúsculas;
+lower - Torna todas as letras em minúsculas;
+capitalize - Joga todos caracteres para minúsculo, deixando apenas a primeira maiuscula;
+title - irá toranar a primeira letra de cada palavra maiuscula;
+strip - Remove espaços inuteis, do inicio e do final da string;
+"""
+#--------------------- PRATICA ------------------------#
+""""
+#### FATIAMENTO ####
+frase = 'Curso em Video Python'
+print(frase[13:])
+
+#### CONTAGEM ####
+frase = 'Curso em Video Python'
+print(frase.count(''))
+"""
+####  ####
