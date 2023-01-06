0d140dfc50c047ff021f0953f085cb22ac616a9b
GuilhermeAngnes <guilhermeangnes13@gmail.com>
2023-01-05 21:52:19 -0300

    Aula10 com execício

---
+++ Aulas/Aula10.py
@@ -0,0 +1,20 @@
+"""
+Exemplos práticos
+"""
+
+"""
+nome = str(input('Qual seu nome? '))
+if nome == 'Guilherme':
+    print('Nome correto!')
+else:
+    print('Nome incorreto!')
+"""
+"""
+n1 = float(input('Qual a primeira nota: '))
+n2 = float(input('Qual a segunda nota: '))
+media = (n1 + n2)/2
+if media >= 7.00:
+    print('Sua Média foi {} \nVocê passou! \nParabéns !!!'.format(media))
+else:
+    print('Sua Média foi {} \nVocê Rodou!'.format(media))
+"""
